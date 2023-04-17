7804fb996af2b34795d42574fdbf320773b0777d
raghav <raghavjindal0212@gmail.com>
2023-04-18 00:36:40 +0530

    changes

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,17 +2,29 @@ import styles from "../style";
 import Button from "./Button";
 
 const CTA = () => (
-  <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+  <section
+    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+  >
     <div className="flex-1 flex flex-col">
       <h2 className={styles.heading2}>Let’s try our service now!</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Everything you need to accept card payments and grow your business
-        anywhere on the planet.
+        Join our community now! Start gaining the ultimate experience of recruitment with TA GURU.
       </p>
     </div>
 
     <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
-      <Button />
+      <button
+        type="button"
+        className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+      >
+        Book a consultation
+      </button>
+      <button
+        type="button"
+        className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+      >
+        Contact us
+      </button>
     </div>
   </section>
 );
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -6,15 +6,18 @@ const CardDeal = () => (
   <section className={layout.section}>
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>
-        Find a better card deal <br className="sm:block hidden" /> in few easy
-        steps.
+        Introducing TAG Tokens
+        {/* <br className="sm:block hidden" /> in few easy
+        steps. */}
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
-        aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+        Candidates will be rewarded with TAG tokens for sharing their resumes. TAG tokens can be used as currency for
+        purchasing goods and services with TA Guru or exchanged with other cryptocurrencies. TAG tokens benefit Clients
+        and Recruiters by reducing recruitment costs, Improving trust, and enhancing the credibility of their profiles
+        on the blockchain.
       </p>
 
-      <Button styles={`mt-10`} />
+      {/* <Button styles={`mt-10`} /> */}
     </div>
 
     <div className={layout.sectionImg}>
