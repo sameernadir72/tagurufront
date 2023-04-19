d19aa61e0ae37d01c2c0d80e1417fe4657db9a2a
raghav <raghavjindal0212@gmail.com>
2023-04-19 12:24:27 +0530

    bug fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,7 @@ import { TypeAnimation } from "react-type-animation";
 
 const Hero = () => {
   return (
-    <section
-      style={{
-        background: "rgb(108,42,198)",
-        background: "linear-gradient(90deg, rgba(108,42,198,1) 31%, rgba(72,35,143,1) 55%, rgba(13,33,69,1) 100%)",
-      }}
-      id="home"
-      className={`flex md:flex-row flex-col ${styles.paddingY}`}
-    >
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row items-center py-[6px] px-4  rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
